b2235b5ada25a09667462eccd2579c3fb3b7fd3c
Create Unity Project <ryohei@R-NakanonoMacBook-Pro.local>
2017-12-10 12:49:08 +0900

    GameObject作成

---
+++ Assets/Scripts/BurrelController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurrelController : MonoBehaviour {
+
+	// 位置座標
+	private Vector3 position;
+	// スクリーン座標をワールド座標に変換した位置座標
+	private Vector3 screenToWorldPointPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		position = Input.mousePosition;//マウスの位置座標を変数positionに格納！
+		position.z = 0;// Z軸修正
+		screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(position);// positionをスクリーン座標→ワールド座標に変換！
+		// ワールド座標に変換されたマウス座標を代入
+		gameObject.transform.position = screenToWorldPointPosition;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField] float moveSpeed;//移動速度
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move ();
+	}
+
+	//Playerの移動に関する関数
+	void Move()
+	{
+		if(Input.GetKey("right"))
+		{
+			this.transform.position += new Vector3 (-moveSpeed * Time.deltaTime, 0, 0);
+		}
+
+		if(Input.GetKey("left"))
+		{
+			this.transform.position += new Vector3 (moveSpeed * Time.deltaTime, 0, 0);
+		}
+
+		if(Input.GetKey("up"))
+		{
+			this.transform.position += new Vector3 (0, moveSpeed * Time.deltaTime, 0);
+		}
+
+		if(Input.GetKey("down"))
+		{
+			this.transform.position += new Vector3 (0, -moveSpeed * Time.deltaTime, 0);
+		}
+	}
+}
